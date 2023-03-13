fe238ba7c1e9fe5b0432128f05484bdaef12b924
Miklós Ferencz <miklos.ferencz@gmail.com>
2023-03-13 16:26:31 +0100

    Create function.php

---
+++ templates/function.php
@@ -0,0 +1,12 @@
+<?php 
+// 💡 There isn't a reason to close the app tag if we're not going to add HTML.
+echo 'functions file';
+
+
+// Variable declarations
+
+
+// Include statements
+
+
+// Hoooks
