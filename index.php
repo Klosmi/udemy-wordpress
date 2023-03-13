9be9f57cf8989717f4b15de451bbde7804b8b922
Miklós Ferencz <miklos.ferencz@gmail.com>
2023-03-13 15:24:29 +0100

    Initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset= <?php bloginfo('charset'); ?>>
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class('exampleclass'); ?>>
+  <?php wp_body_open(); ?>
+  
+  <h1>Lorem ipsum dolor</h1>
+  
+  <?php wp_footer(); ?>
+</body>
+</html>
+
+ 
